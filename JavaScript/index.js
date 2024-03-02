1fac8d5a99624516fe7d791e6c6e15bbdd955743
BradLeneus <bradleyleneus5@gmail.com>
2024-03-02 15:22:39 -0500

    update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,30 +1,30 @@
 ////////////////// Obtenez le modal d'inscription ///////////////////////    
-function login (event){  // fonction pour se connecter
+function login(event) {  // fonction pour se connecter
   event.preventDefault();
   const nomUtilisateur = document.getElementById("unameLogin").value;
   const pwsd = document.getElementById("pswdLogin").value;
- 
+
   const connexionUtilisateur = localStorage.getItem("uname");
   const pwsdLogin = localStorage.getItem("pswd");
- 
-  if(nomUtilisateur === connexionUtilisateur && pwsd === pwsdLogin){
-      window.location.replace("../accueil.html")
+
+  if (nomUtilisateur === connexionUtilisateur && pwsd === pwsdLogin) {
+    window.location.replace("../accueil.html")
   }
-  else{
+  else {
     alert("Désolé,Vous n'avez pas entré vos bonnes informations.")
   }
 }
- 
-function inscrireUtilisateur(){ // fonction pour s'inscrire
+
+function inscrireUtilisateur() { // fonction pour s'inscrire
   const nomUtilisateur = document.getElementById("nomUtilisateur").value;
   const mdp = document.getElementById("mdp").value;
   const confirmationMdp = document.getElementById("confirmationMdp").value;
- 
-  if(mdp === confirmationMdp){
+
+  if (mdp === confirmationMdp) {
     localStorage.setItem("uname", nomUtilisateur)
     localStorage.setItem("pswd", mdp)
   }
-  }
+}
 
 
 
@@ -33,7 +33,7 @@ function inscrireUtilisateur(){ // fonction pour s'inscrire
 var modal = document.getElementById('id01');
 
 // Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
-window.onclick = function(event) {
+window.onclick = function (event) {
 
   if (event.target == modal) {
     modal.style.display = "none";
@@ -45,8 +45,8 @@ window.onclick = function(event) {
 var modal2 = document.getElementById('id02');
 
 // Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
-window.onclick = function(event) {
-    if (event.target == modal2) {
-        modal.style.display = "none";
-    }
+window.onclick = function (event) {
+  if (event.target == modal2) {
+    modal.style.display = "none";
+  }
 }
